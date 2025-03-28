58e65d4a66c7e9c2f4e7aa3c08f6b0e9c6b76b54
Anyidollar <ifeanyionyekwere247@gmail.com>
2025-03-28 12:30:23 +0100

    Implement Navbar

---
+++ src/components/NavIcons.tsx
@@ -0,0 +1,6 @@
+"use client";
+const Navicons = () => {
+  return <div>Nav Icons</div>;
+};
+
+export default Navicons;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import Menu from "./Menu";
+import Image from "next/image";
+import SearchBar from "./SearchBar";
+import Navicons from "./Navicons";
 
-const NavBar = () => {
+const Navbar = () => {
   return (
-    <div className=''>
-     Navbar
+    <div className="h-20 px-4 md:px-8 lg:px-16 xl:32 2xl:64 relative">
+      {/* MOBILE */}
+      <div className="h-full flex items-center justify-between md:hidden">
+        <Link href="/">
+          <div className="text-2xl tracking-wide">Anyidollar</div>
+        </Link>
+        <Menu />
+      </div>
+      {/* BIGGER SCREENS */}
+      <div className="hidden md:flex items-center h-full justify-between gap-8">
+        {/* LEFT */}
+        <div className="w-1/3">
+          <Link href="/" className="flex items-center gap-3">
+            <Image src="/logo.png" alt="" width={24} height={24} />
+            <div className="text-2xl tracking-wide">Anyidollar</div>
+          </Link>
+        </div>
+        {/* RIGHT */}
+        <div className="w-2/3 flex items-center justify-between gap-8">
+          <SearchBar />
+          <Navicons />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default Navbar;
