c750c7205a0f05ac4041058bb6a89db733b96209
Anyidollar <ifeanyionyekwere247@gmail.com>
2025-04-01 07:11:13 +0100

    Implemented Footer section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,44 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
-  return <div className="">Footer</div>;
+  return (
+    <div className="py-24 px-4 md:px-8 lg:px-16 xl:32 2xl:64 bg-gray-100 text-sm mt-24">
+      {/* TOP */}
+      <div className="flex justify-between gap-24">
+        {/* LEFT */}
+        <div className="w-1/4 flex flex-col gap-8">
+          <Link href="">
+            <div className="text-2xl tracking-wide">Anyidollar</div>
+          </Link>
+          <p>
+            3252 Winding Way, Central Plaza, Willowbrook, CA 90210, United
+            States
+          </p>
+          <span className="font-semibold">ifeanyionyekwere30@gmail.com</span>
+          <span className="font-semibold">+234-7063400111</span>
+          <div className="flex gap-6">
+            <Image src="/facebook.png" alt="" width={16} height={16} />
+            <Image src="/instagram.png" alt="" width={16} height={16} />
+            <Image src="/youtube.png" alt="" width={16} height={16} />
+            <Image src="/pininterest.png" alt="" width={16} height={16} />
+            <Image src="/x.png" alt="" width={16} height={16} />
+          </div>
+        </div>
+      </div>
+      <div className="">
+        {/* CENTER */}
+        <div className="w-1/2"></div>
+      </div>
+      <div className="">
+        {/* RIGHT */}
+        <div className="w-1/4"></div>
+      </div>
+      {/* BOTTOM */}
+      <div className=""></div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -3,14 +3,122 @@ import Link from "next/link";
 
 const ProductList = () => {
   return (
-    <div className="flex gap-x-8 gap-y-16 justify-between flex-wrap">
-      <Link href="/test" className="relative w-full h-80">
-        <Image
-          src="https://plus.unsplash.com/premium_photo-1711051475117-f3a4d3ff6778?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt=""
-          fill
-          sizes="25vw"
-        />
+    <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://plus.unsplash.com/premium_photo-1711051475117-f3a4d3ff6778?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Names</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">Description</div>
+        <button className="rounded-2xl ring-1 ring-ifeanyi w-max text-ifeanyi py-2 px-4 text-xs hover:bg-ifeanyi hover:text-white">
+          Add to Cart
+        </button>
+      </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://plus.unsplash.com/premium_photo-1711051475117-f3a4d3ff6778?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Names</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">Description</div>
+        <button className="rounded-2xl ring-1 ring-ifeanyi w-max text-ifeanyi py-2 px-4 text-xs hover:bg-ifeanyi hover:text-white">
+          Add to Cart
+        </button>
+      </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://plus.unsplash.com/premium_photo-1711051475117-f3a4d3ff6778?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Names</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">Description</div>
+        <button className="rounded-2xl ring-1 ring-ifeanyi w-max text-ifeanyi py-2 px-4 text-xs hover:bg-ifeanyi hover:text-white">
+          Add to Cart
+        </button>
+      </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://plus.unsplash.com/premium_photo-1711051475117-f3a4d3ff6778?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Names</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">Description</div>
+        <button className="rounded-2xl ring-1 ring-ifeanyi w-max text-ifeanyi py-2 px-4 text-xs hover:bg-ifeanyi hover:text-white">
+          Add to Cart
+        </button>
       </Link>
     </div>
   );
