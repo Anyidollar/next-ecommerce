edd997d7cddcba6705722cb92ccfbd419950c8c5
Anyidollar <ifeanyionyekwere247@gmail.com>
2025-04-07 18:06:23 +0100

    Implement cartModal component

--- src/components/CustomizeProducts.tsx
+++ src/components/CustomizeProducts.tsx
@@ -13,7 +13,15 @@ const CustomizeProducts = () => {
       </ul>
       <h4 className="font-medium">Choose a size</h4>
       <ul className="flex items-center gap-3">
-        <li className="ring-1 ring-ifeanyi text-ifeanyi rounded-md py-1 px-4 text-sm cursor-pointer">Small</li>
+        <li className="ring-1 ring-ifeanyi text-ifeanyi rounded-md py-1 px-4 text-sm cursor-pointer">
+          Small
+        </li>
+        <li className="ring-1 ring-ifeanyi text-white bg-ifeanyi rounded-md py-1 px-4 text-sm cursor-pointer">
+          Medium
+        </li>
+        <li className="ring-1 ring-pink text-white bg-pink-200 rounded-md py-1 px-4 text-sm cursor-not-allowed">
+          Large
+        </li>
       </ul>
     </div>
   );
